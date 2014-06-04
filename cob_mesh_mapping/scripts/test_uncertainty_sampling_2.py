d8d572024264d9d7481fc19f4e0eac4be48f52f6
ipa-goa-sf <st.fuchs.tr@gmail.com>
2014-06-04 16:03:00 +0200

    added second sample set for joint dist

--- cob_mesh_mapping/scripts/test_uncertainty_sampling_2.py
+++ cob_mesh_mapping/scripts/test_uncertainty_sampling_2.py
@@ -11,42 +11,57 @@ def weight(x,s):
 
 sensor = sm.SensorModel()
 nn = 10
-beta_true = mat(random.rand(3,1))
-beta_true = mat([-1.,1.2,-0.25]).T
-beta_true = beta_true/linalg.norm(beta_true)
-samples = mat(hstack([2.*random.rand(nn,1)+1.,zeros([nn,1]),ones([nn,1])]))
+beta_true1 = mat([-1.,1.2,-0.25]).T
+beta_true1 = beta_true1/linalg.norm(beta_true1)
+samples1 = mat(hstack([2.*random.rand(nn,1)+1.,zeros([nn,1]),ones([nn,1])]))
+
+beta_true2 = mat([1.,.7,-2.25]).T
+beta_true2 = beta_true2/linalg.norm(beta_true2)
+samples2 = mat(hstack([2.*random.rand(nn,1)-1.,zeros([nn,1]),ones([nn,1])]))
 for i in range(nn):
-    samples[i,1] = samples[i]*beta_true / -beta_true[1]
-    samples[i,1] = samples[i,1] + weight(samples[i,1],sensor)*random.randn()
+    samples1[i,1] = samples1[i]*beta_true1 / -beta_true1[1]
+    samples1[i,1] = samples1[i,1] + weight(samples1[i,1],sensor)*random.randn()
+    samples2[i,1] = samples2[i]*beta_true2 / -beta_true2[1]
+    samples2[i,1] = samples2[i,1] + weight(samples2[i,1],sensor)*random.randn()
 
 A = mat(zeros([3,3]))
 sig2 = mat(zeros([nn,1]))
+B = mat(zeros([3,3]))
 
 for i in range(nn):
-    x = samples[i].T
+    x = samples1[i].T
     sig2[i] = weight(x[1],sensor)
     A = A + x*x.T / sig2[i]
+    x = samples2[i].T
+    B = B + x*x.T / weight(x[1],sensor)
 
 Ainv = linalg.inv(A)
+Binv = linalg.inv(B)
 mu = Ainv[:-1,-1] / Ainv[-1,-1] # A_12 / A_22
 grad = vstack([mu,mat(1.)])
 grad = grad / linalg.norm(grad)
 
 # fill grid in euclidean space (x1,x2)
-X1,X2 = meshgrid(linspace(0,3,50),linspace(0,3,50))
+X1,X2 = meshgrid(linspace(-1,3,80),linspace(0,5,80))
 F_grad = zeros(shape(X1))
+det1 = zeros(shape(X1))
+det2 = zeros(shape(X1))
 for yi in range(shape(X1)[0]):
     for xi in range(shape(X1)[1]):
         x = mat([[X1[yi,xi]],[X2[yi,xi]],[1.]])
         F_grad[yi,xi] = x.T*grad
+        det1[yi,xi] = ( sqrt( (2.*pi)**3 * linalg.det(linalg.inv(x*x.T + A)))
+                       + sqrt( (2.*pi)**3 * linalg.det(linalg.inv(x*x.T + B))) )
+        det2[yi,xi] = 0.5*(exp(-0.5*x.T*Ainv*x)/(sqrt((2.*pi)**3*linalg.det(A)))
+                           +exp(-0.5*x.T*Binv*x)/(sqrt((2.*pi)**3*linalg.det(B))))*1000.
 
 
 nnn = 50
 mus = zeros(nnn)
 conf_p = zeros([nnn,3])
 conf_n = zeros([nnn,3])
-xmin = samples[:,0].min()
-xmax = samples[:,0].max()
+xmin = min(samples1[:,0].min(),samples2[:,0].min())
+xmax = max(samples1[:,0].max(),samples2[:,0].max())
 samples_new = mat(hstack([mat(linspace(xmin,xmax,nnn)).T,zeros([nnn,1]),ones([nnn,1])]))
 for i in range(nnn):
     samples_new[i,1] = samples_new[i]*grad / -grad[1]
@@ -78,12 +93,22 @@ for i in range(shape(phi)[0]):
 
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(111)
-cs1 = ax1.contour(X1,X2,F_grad,20)
+#cs1 = ax1.contour(X1,X2,F_grad,20)
+cs1 = ax1.contour(X1,X2,det1,20)
 ax1.clabel(cs1,inline=1,fontsize=10)
-ax1.plot(samples[:,0],samples[:,1], 'x')
-ax1.plot(conf_p[:,0],conf_p[:,1],'r')
-ax1.plot(conf_n[:,0],conf_n[:,1],'r')
+ax1.plot(samples1[:,0],samples1[:,1], 'x')
+ax1.plot(samples2[:,0],samples2[:,1], 'x')
+#ax1.plot(conf_p[:,0],conf_p[:,1],'r')
+#ax1.plot(conf_n[:,0],conf_n[:,1],'r')
 ax1.grid()
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+cs2 = ax2.contour(X1,X2,det2,20)
+ax2.clabel(cs2,inline=1,fontsize=10)
+ax2.plot(samples1[:,0],samples1[:,1], 'x')
+ax2.plot(samples2[:,0],samples2[:,1], 'x')
+ax2.grid()
+
 
 '''
 fig2 = plt.figure()
